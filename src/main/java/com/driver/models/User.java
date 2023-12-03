448e0b96f59dc2046b2c9d42343a3902d89184b7
= <=>
2023-12-03 11:50:47 +0530

    solving user last3

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -12,7 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false)
+    @Column(unique = true)
     private String username;
 
     private String password;
